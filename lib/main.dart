f28197eb08129a9f2ff2b7fe88da53ca7dd37620
André Pelisoli <andre.pelisoli@somoseducacao.com.br>
2020-08-25 23:54:37 -0300

    Change QuizApp to extends StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-main () => runApp(QuizApp());
+main() => runApp(QuizApp());
 
+class _QuizAppState extends State<QuizApp> {
+  var _selectedAnswer = 0;
 
-class QuizApp extends StatelessWidget {
+  void _answer() {
+    setState(() {
+      _selectedAnswer++;
+    });
+    print(_selectedAnswer);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Quiz App'),
-          centerTitle: true,
-        ),
-        body: Text('Hello Flutter'),
-      )
-    );
-  }
+    final questions = [
+      'What is your favorite color?',
+      'What is your favorite animal?',
+    ];
 
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Quiz App'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
+          Text(questions[_selectedAnswer]),
+          RaisedButton(
+            child: Text('Answer 1'),
+            onPressed: _answer,
+          ),
+          RaisedButton(
+            child: Text('Answer 2'),
+            onPressed: _answer,
+          ),
+          RaisedButton(
+            child: Text('Answer 3'),
+            onPressed: _answer,
+          )
+        ],
+      ),
+    ));
+  }
+}
+
+class QuizApp extends StatefulWidget {
+  _QuizAppState createState() {
+    return _QuizAppState();
+  }
 }
